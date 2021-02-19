484d9c47a0a14583bbadf2fd00b5a10339613437
Bohdan <bohdan.bilous2@gmail.com>
2021-02-19 13:04:35 +0200

    config fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import StylelintWebpackPlugin from 'stylelint-webpack-plugin'
 
 export default {
   mode: 'universal',
-  target: 'server',
+  target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     htmlAttrs: {
